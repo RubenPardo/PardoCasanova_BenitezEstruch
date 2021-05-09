996f0f08689c2c2b4b4b6a75c54c864ab6e5c612
Ruben <rubenpardocasanova@gmail.com>
2021-05-09 11:21:52 +0200

    Client Logic

--- Hello World/ClientLogic/ClientLogic.cs
+++ Hello World/ClientLogic/ClientLogic.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WebPage.localhost;
 
 namespace WebPage.ClientLogic
 {
     public class ClientLogic
     {
+        WebService1 wb;
+        public ClientLogic()
+        {
+            wb = new WebService1();
+        }
     }
 }
